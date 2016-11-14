522351592bb8e764d5799ca5239c505f5a8277f2
Vári-Kakas <varikakasandor@gmail.com>
2016-11-14 13:57:23 +0100

    Feltölt!

---
+++ 1-100szamok.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=100;i++)
+    {
+        if(i%3==0)
+        {
+            cout<<"FIZZ"<<" ";
+        }
+        else
+        {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
